50f907ab8ce6df9c184f6bbeb5097efa626885b4
Daniel Cumbor <dcdolphy@gmail.com>
2020-11-25 19:22:54 +0000

    Added and linked SDL2 dependencies

--- Invaders3D/main.cpp
+++ Invaders3D/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <SDL.h>
 
 // Written by Daniel Cumbor in 2020. Thanks Covid-19!
 
-int main()
+int main(int argc, char** argv)
 {
 	std::cout << "Hello, World!" << std::endl;
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init Error: %s\n", SDL_GetError());
+	}
+	SDL_Log("SDL initialised OK!\n");
 
 	return 1;
 }
