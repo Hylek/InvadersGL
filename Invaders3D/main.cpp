8eb6982148018f56937b0c63b4d208f02e743f7d
Daniel Cumbor <dcdolphy@gmail.com>
2020-11-25 19:34:14 +0000

    Added and linked SDL2 mixer dependencies

--- Invaders3D/main.cpp
+++ Invaders3D/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_mixer.h>
 
 // Written by Daniel Cumbor in 2020. Thanks Covid-19!
 
@@ -13,6 +14,11 @@ int main(int argc, char** argv)
 		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init Error: %s\n", SDL_GetError());
 	}
 	SDL_Log("SDL initialised OK!\n");
+
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+	{
+		SDL_Log("Mixer could not start: %s\n", SDL_GetError());
+	}
 
 	return 1;
 }
